3713a491e5ddf5394c4c2a371c71ee39c7f8ae50
Gideon5348 <gideonmutai096@gmail.com>
2023-10-21 10:11:02 +0300

    0. Simply matching School

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,20 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if the argument matches the regular expression
-def match_string(arg)
-  regex = /School/i # Regular expression to match "School" case-insensitively
-  if regex.match(arg)
-    puts arg.gsub(regex, 'School') # Replace matches with "School"
-  else
-    puts "$" # No match found
-  end
-end
-
-# Check if there is an argument provided
-if ARGV.length != 1
-  puts "Usage: #{$0} <string>"
-  exit(1)
-end
-
-# Call the match_string function with the provided argument
-match_string(ARGV[0])
+puts ARGV[0].scan(/School/).join
