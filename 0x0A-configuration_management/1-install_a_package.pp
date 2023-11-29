a878e01ad701d5876a4c1ee216b6372a45849e2e
Gideon5348 <gideonmutai096@gmail.com>
2023-11-29 16:26:15 +0300

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,7 @@
 #!/usr/bin/pup
 # Puppet manifest to install Flask version 2.1.0 using pip3
 
-class { 'python':
-  version => 'system',
-}
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
-package { 'Flask':
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
