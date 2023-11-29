d73fc63079e726f1761eca005b9780abdd02e004
Gideon5348 <gideonmutai096@gmail.com>
2023-11-29 17:51:35 +0300

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-#This code will install the package pupet-lint
+# This code will install the Flask package with version 2.1.0 using the pip3 provider
+
 package { 'flask':
-ensure  =>  '2.1.0',
-provider  =>  'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
