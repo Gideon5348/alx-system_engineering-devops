c15649f5d597135ecb3a851145b0ac3e06102c02
Gideon5348 <gideonmutai096@gmail.com>
2023-11-29 13:22:01 +0300

    4. Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,5 @@
 # 100-puppet_ssh_config.pp
 
-file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/sshd_config',
-  line  => 'PasswordAuthentication no',
-}
-
 file_line { 'Declare identity file':
   path  => '/home/gideon/.ssh/config',
   line  => '    IdentityFile ~/.ssh/school',
