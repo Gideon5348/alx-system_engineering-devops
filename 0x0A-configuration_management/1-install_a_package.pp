364b644f1785f70ebda94d03f48a43ab10352fa7
Gideon5348 <gideonmutai096@gmail.com>
2023-12-21 15:52:42 +0300

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# This code will install the Flask package with version 2.1.0 using the pip3 provider
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Install Flask
+package {'flask':
+ensure   => '2.1.0',
+provider => 'pip3',
 }
