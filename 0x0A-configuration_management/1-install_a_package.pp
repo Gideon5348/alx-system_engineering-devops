05e66d838a17eb73a6cb9ee7bfd780cf45cbfac4
root <Gideon5348>
2024-03-18 07:17:04 -0700

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
-# Install flask
+#!/usr/bin/pup
+# Ensure python3-pip is installed
+package { 'python3-pip':
+  ensure  => installed,
+}
+
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.0.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
