a7ea0224e21b6dc9f6b8b570a62036bc6e9a9f18
root <Gideon5348>
2024-04-10 14:56:55 -0700

    0. Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -4,25 +4,7 @@
 #
 # Author: Gideon Mutai
 
-# Example resource types to fix the issue
-file { '/var/www/html/wp-settings.php':
-  ensure  => file,
-  content => file('/var/www/html/wp-settings.php'), # Read the content of wp-settings.php
-  replace => 'true',                               # Replace the content when it matches
-  notify  => Exec['restart_apache'],                # Notify restart_apache when changes are made
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
-
-exec { 'restart_apache':
-  command     => '/etc/init.d/apache2 restart',
-  refreshonly => true,
-}
-
-service { 'apache2':
-  ensure  => running,
-  enable  => true,
-  require => Exec['restart_apache'],
-}
-
